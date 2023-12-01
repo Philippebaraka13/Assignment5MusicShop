157fa9cf1db03c4fdc538e2df11f0f711f96036c
philippe baraka <kamokamophilippe13@gmail.com>
2023-12-01 17:42:28 -0600

    Add project files.

---
+++ Data/Assignment5MusicShopContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assignment5MusicShop.Models;
+
+namespace Assignment5MusicShop.Data
+{
+    public class Assignment5MusicShopContext : DbContext
+    {
+        public Assignment5MusicShopContext (DbContextOptions<Assignment5MusicShopContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment5MusicShop.Models.Music> Music { get; set; } = default!;
+    }
+}
---
+++ Models/Music.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment5MusicShop.Models
+{
+    public class Music
+    {
+        public required int Id { get; set; }
+
+        public required string Title { get; set; }
+
+        public required string Performer { get; set; }
+        public required string Genre { get; set; }
+        public decimal Price { get; set; }
+        public int Year {  get; set; }
+
+
+    }
+}
